aee025b064e30f6cf2b6ba57c480b497eb14d61f
bojack <g@g.gg>
2017-06-12 11:38:47 +0800

    remove redundant custom networks definition

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -64,23 +64,7 @@ angular.module('copayApp.services').factory('configService', function(appConfigS
 
     emailNotifications: {
       enabled: false,
-    },
-
-    customNetworks: [
-      {
-        name: 'Aureus',
-        alias: 'AUREUS',
-        code: 'aur',
-        symbol: 'AUR',
-        pubkeyhash: 0x17,
-        privatekey: 0x80,
-        scripthash: 0x1C,
-        xpubkey: 0x0488b21e,
-        xprivkey: 0x0488ade4,
-        bwsUrl: 'https://bws.aureus.cc/bws/api',
-        port: 9697,
-        magic: 0x6ee58c2a
-      }
+    }
     ]
   };
 
