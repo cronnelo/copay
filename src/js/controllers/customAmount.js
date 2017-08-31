092b9237d1c964b74042de4a0c03c72ec9ef122d
hiram <hiramabiff@eastgate.com>
2017-08-31 19:30:50 +0800

    rate service dump1

--- src/js/controllers/customAmount.js
+++ src/js/controllers/customAmount.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('customAmountController', function($rootScope, $scope, $stateParams, $ionicHistory, txFormatService, platformInfo, profileService) {
+angular.module('copayApp.controllers').controller('customAmountController', function($rootScope, $scope, $stateParams, $ionicHistory, txFormatService, platformInfo, profileService, CUSTOMNETWORKS) {
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
     var satToBtc = 1 / 100000000;
@@ -9,9 +9,9 @@ angular.module('copayApp.controllers').controller('customAmountController', func
     $scope.amount = parseInt(data.stateParams.toAmount);
     $scope.amountBtc = ($scope.amount * satToBtc).toFixed(8);
     $scope.amountStr = txFormatService.formatAmountStr($scope.amount);
-    $scope.altAmountStr = txFormatService.formatAlternativeStr($scope.amount);
     $scope.wallet = profileService.getWallet($stateParams.walletId);
     $scope.network = $scope.wallet.network;
+    $scope.altAmountStr = txFormatService.formatAlternativeStr($scope.amount, CUSTOMNETWORKS[$scope.network]);
     if($scope.network === "livenet") {$scope.network = "bitcoin";}
   });
 
