2e59b105979042b6f4adbec25fe0047ec122c388
Ronnel Ocampo <ocampo.ronnelc@gmail.com>
2017-09-28 21:32:44 +0800

    REFACTOR - Cleaner walletIcon directive

--- src/js/directives/walletIcon.js
+++ src/js/directives/walletIcon.js
@@ -1,35 +1,48 @@
 'use strict';
 
 angular.module('copayApp.directives')
-  .directive('walletIcon', function($rootScope, $timeout) {
-    return {
+  .directive('walletIcon', function() {
+    var directive = {
       restrict: 'A',
       templateUrl: 'views/includes/walletIcon.html',
-      scope: {wallet: '=', network: '='},
-
-      link: function(scope, element, attrs) {
-        
-        scope.$watch("wallet",function(newValue,oldValue) {
-          updateScope();
-        });
-        scope.$watch("network",function(newValue,oldValue) {
-          updateScope();
-        }); 
-        function updateScope() {       
-          scope.bitlox = false;
-          var v = scope.wallet;
-          if(scope.wallet && typeof(scope.wallet.isPrivKeyExternal) === 'function' && scope.wallet.isPrivKeyExternal() && scope.wallet.getPrivKeyExternalSourceName().indexOf('bitlox') > -1) {
-            scope.bitlox = true;
-          } else if (scope.wallet && scope.wallet.isPrivKeyExternalString && scope.wallet.getPrivKeyExternalSourceNameString.indexOf('bitlox') > -1) {
-            scope.bitlox = true
-          } 
-
-          if (!scope.wallet && scope.network) {
-            scope.wallet = {network: scope.network}
-          }      
-        }
-        updateScope();
-
-      }
+      scope: {
+        wallet: '=',
+        network: '='
+      },
+      link: link
     };
+
+    return directive;
+
+    function link(scope) {
+      scope.$watch("wallet", function() {
+        updateScope();
+      });
+
+      scope.$watch("network", function() {
+        updateScope();
+      });
+
+      updateScope();
+
+      function updateScope() {
+        scope.bitlox = false;
+
+        if(scope.wallet
+          && typeof(scope.wallet.isPrivKeyExternal) === 'function'
+          && scope.wallet.isPrivKeyExternal()
+          && scope.wallet.getPrivKeyExternalSourceName().indexOf('bitlox') > -1) {
+          scope.bitlox = true;
+        } else if (scope.wallet
+          && scope.wallet.isPrivKeyExternalString
+          && scope.wallet.getPrivKeyExternalSourceNameString.indexOf('bitlox') > -1) {
+          scope.bitlox = true
+        }
+
+        if (!scope.wallet && scope.network) {
+          scope.wallet = { network: scope.network }
+        }
+      }
+    }
+
   });
