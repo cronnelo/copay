bee0624ba1bb371ed0e4b2304a9f7d3cd700f1f9
Ronnel Ocampo <ocampo.ronnelc@gmail.com>
2017-09-29 19:49:09 +0800

    ENHANCEMENT - Create service for ordering wallets

---
+++ src/js/services/orderedWallet.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular
+  .module('copayApp.services')
+  .factory('orderedWallet', function(storageService, lodash) {
+    var service = {
+      arrange: arrange
+    };
+
+    return service;
+
+    //////////////
+
+    /**
+     * @param {[ Object ]} wallets Arrange wallets passed
+     *                             Passed by reference - it will change the original wallets
+     */
+    function arrange(wallets, callback) {
+      storageService.getOrderedWallet(function(error, orderOfWallets) {
+        orderOfWallets = orderOfWallets
+                       ? JSON.parse(orderOfWallets)
+                       : createOrderedWallets(wallets);
+
+        lodash.forEach(orderOfWallets, function(wallet, index) {
+          var walletIndex = lodash.findIndex(wallets, function(o) {
+            return o.name === wallet;
+          });
+
+          wallets.splice(index, 0, wallets.splice(walletIndex, 1)[0]);
+        });
+
+        callback(wallets);
+      });
+    }
+
+    function createOrderedWallets(wallets) {
+      return wallets.map(function(wallet) {
+        return wallet.name;
+      });
+    }
+  });
