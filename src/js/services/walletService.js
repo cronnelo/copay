b8aaa7750805baeb1947efb49088a1a747473a3c
Winscame Ye <hugosthere@MacBook-Air.local>
2018-01-21 15:01:06 +0800

    changed text string and destroy listener

--- src/js/services/walletService.js
+++ src/js/services/walletService.js
@@ -138,7 +138,7 @@ angular.module('copayApp.services').factory('walletService', function($log, $tim
         tx.wallet = wallet;
 
         if (!tx.wallet) {
-          $log.error("no wallet at txp?");
+          $log.log("no wallet at txp?");
           return;
         }
 
@@ -614,7 +614,7 @@ angular.module('copayApp.services').factory('walletService', function($log, $tim
 
     storageService.removeTxHistory(wallet.id, function(err) {
       if (err) {
-        $log.error(err);
+        $log.log(err);
         return cb(err);
       }
       return cb();
@@ -711,7 +711,7 @@ angular.module('copayApp.services').factory('walletService', function($log, $tim
           return _signWithIntelTEE(wallet, txp, cb);
         default:
           var msg = 'Unsupported External Key:' + wallet.getPrivKeyExternalSourceName();
-          $log.error(msg);
+          $log.log(msg);
           return cb(msg);
       }
     } else {
@@ -1147,10 +1147,11 @@ angular.module('copayApp.services').factory('walletService', function($log, $tim
           ongoingProcess.set('signingTx', false, customStatusHandler);
           if (err) {
             $ionicLoading.hide();
+            $rootScope.destroyBitloxListeners();
             $log.warn('sign error:', err);
             var msg = err && err.message ?
               err.message :
-              gettextCatalog.getString('The payment was created but could not be completed. Please try again from home screen');
+              gettextCatalog.getString('The payment was canceled or rejected by the user, or the BitLox was disconnected.');
 
             $rootScope.$emit('Local/TxAction', wallet.id);
             return cb(msg);
@@ -1175,7 +1176,7 @@ angular.module('copayApp.services').factory('walletService', function($log, $tim
 
           root.invalidateCache(wallet);
 
-
+          $rootScope.destroyBitloxListeners();
           if (signedTxp.status == 'accepted') {
             ongoingProcess.set('broadcastingTx', true, customStatusHandler);
             root.broadcastTx(wallet, signedTxp, function(err, broadcastedTxp) {
